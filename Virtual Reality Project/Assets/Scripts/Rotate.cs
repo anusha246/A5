1db4824e8320ea4b9afaf416be8b74d23956af02
wongry22 <ryane.wong@mail.utoronto.ca>
2019-12-02 09:27:45 -0500

    added trash animations

--- Virtual Reality Project/Assets/Scripts/Rotate.cs
+++ Virtual Reality Project/Assets/Scripts/Rotate.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0f, -0.10f, 0f));
     }
 }
